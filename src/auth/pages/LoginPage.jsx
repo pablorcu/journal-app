0f7549b4204987e1f17d1053ef52fe42bf1269ec
Pablo Campoverde <pcampoverde73@hotmail.com>
2024-03-14 23:09:03 -0500

    first commit

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { Typography }  from '@mui/material'
+import { MailOutline } from '@mui/icons-material'
+
+export const LoginPage = () => {
+  return (
+    <>
+      <Typography variant='h1'>LoginPage</Typography>
+      <MailOutline />
+    </>
+  )
+}
